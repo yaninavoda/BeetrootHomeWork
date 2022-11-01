d7a15649af76cc56e55a7e6ca4fb553aced00311
Ianina Voda <yaninavoda@gmail.com>
2022-11-01 11:30:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BeetrootHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
