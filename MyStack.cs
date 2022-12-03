b621d5728bfb518db6a78d2d31bec12f67293439
Ianina Voda <yaninavoda@gmail.com>
2022-12-03 13:47:17 +0200

    Added mock

---
+++ MyStack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeetrootHomework
+{
+    internal class MyStack
+    {
+    }
+}
