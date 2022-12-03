eb73754f22904681fbd75fdb131a544b7c8c80f3
Ianina Voda <yaninavoda@gmail.com>
2022-12-03 19:38:01 +0200

    Created custom MyStack Class

--- MyStack.cs
+++ MyStack.cs
@@ -1,12 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
+using System.Dynamic;
 
 namespace BeetrootHomework
 {
-    internal class MyStack
+    public class MyStack<T> : IEnumerable<T>
     {
+        
+        private List<T> _elements;
+        public int Count { get; set; }
+
+        public MyStack()
+        {
+            Count = _elements.Count;
+        }
+
+        public MyStack(List<T> elements)
+        {
+            _elements = elements;
+            Count = elements.Count;
+        }
+
+        public T PopStack()
+        {
+            T element = _elements[GetLastIndex()];
+
+            _elements.RemoveAt(GetLastIndex());
+
+            Count--;
+
+            return element;
+        }
+
+        public void PushStack(T value)
+        {
+            _elements.Add(value);
+
+            Count++;
+        }
+        public void ClearStack()
+        {
+            _elements.Clear();
+
+            Count = 0;
+        }
+        public T PeekStack()
+        {
+            T element = _elements[GetLastIndex()];
+
+            return element;
+        }
+        public void CopyToStack(T[] arr) 
+        {
+            for (int i = 0; i < _elements.Count; i++)
+            {
+                arr[i] = _elements[i];
+            }
+        }
+
+        public void PrintStack()
+        {
+            foreach (var item in _elements)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            //_elements.Reverse();
+
+            var enumerator = _elements.GetEnumerator();
+            
+
+            return enumerator;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            //_elements.Reverse();
+            var enumerator = _elements.GetEnumerator();
+            
+            return enumerator;
+        }
+
+        private int GetLastIndex()
+        {
+            int index = _elements.Count - 1;
+
+            return index;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,101 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region StringStack
+
+            var listStr = new List<string> { "first", "second", "third" };
+
+            var myStackString = new MyStack<string>(listStr);
+
+            Console.WriteLine("Stack after initialization:");
+            myStackString.PrintStack();
+
+            myStackString.PopStack();
+
+            Console.WriteLine("Stack after Pop:");
+            myStackString.PrintStack();
+
+            myStackString.PushStack("fourth");
+            myStackString.PushStack("fifth");
+
+            Console.WriteLine("Stack after two consequent Pushes:");
+            myStackString.PrintStack();
+
+            Console.WriteLine($"Peek method returns : {myStackString.PeekStack()}");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            var arrayStr = new string[myStackString.Count];
+
+            myStackString.CopyToStack(arrayStr);
+            Console.WriteLine("Stack copied to array looks as follows:");
+            foreach (var item in arrayStr)
+            {
+                Console.WriteLine(item);
+            }
+
+            #endregion
+
+            SwitchConsole();
+
+            #region DoubleStack
+
+            var listDouble = new List<double> { 0, 0.2, 1.1 };
+            var myStackDouble = new MyStack<double>(listDouble);
+
+            Console.WriteLine("Stack after initialization:");
+            myStackDouble.PrintStack();
+
+            myStackDouble.PopStack();
+
+            Console.WriteLine("Stack after Pop:");
+            myStackDouble.PrintStack();
+
+            myStackDouble.PushStack(9.9);
+            myStackDouble.PushStack(3.56);
+
+            Console.WriteLine("Stack after two consequent Pushes:");
+            myStackDouble.PrintStack();
+
+            Console.WriteLine($"Peek method returns : {myStackDouble.PeekStack()}");
+
+            #endregion
+
+            SwitchConsole();
+
+            #region IntStack
+
+            var listInt = new List<int> { 0, 1, 2, 3 };
+
+            var myStackInt = new MyStack<int>(listInt);
+
+            Console.WriteLine("Stack after initialization:");
+            myStackInt.PrintStack();
+
+            myStackInt.PopStack();
+
+            Console.WriteLine("Stack after Pop:");
+            myStackInt.PrintStack();
+
+            myStackInt.PushStack(44);
+            myStackInt.PushStack(55);
+
+            Console.WriteLine("Stack after two consequent Pushes:");
+            myStackInt.PrintStack();
+
+            Console.WriteLine($"Peek method returns : {myStackInt.PeekStack()}");
+
+            #endregion
+
+            Console.ReadKey();
+        }
+
+        public static void SwitchConsole()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key for another data type");
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
