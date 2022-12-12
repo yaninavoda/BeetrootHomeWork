ad99b3cc2e3053072a69161486836e3b9105451c
Ianina Voda <yaninavoda@gmail.com>
2022-12-12 22:37:39 +0200

    Added SnakeGame working version

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                var gameFlow = new GameFlow();
+
+                gameFlow.StartGame();
+
+                Console.ReadKey();
+            }
         }
     }
 }
